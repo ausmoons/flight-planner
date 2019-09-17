5a4c235ecc18ec3c82f0870e87ba53176cb686cd
Liva <liva.zvirgzdina@gmail.com>
2019-09-17 20:39:42 +0300

    Adding Files

---
+++ flight-planner/Controllers/TestingApiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using flight_planner.Models;
+
+namespace flight_planner.Controllers
+{
+    public class TestingApiController : ApiController
+    {
+        [HttpPost]
+        [Route("testing-api/clear")]
+        public bool Clear()
+        {
+            FlightStorage.ClearList();
+            return true;
+        }
+        
+
+        public string Get()
+        {
+            return "Testing api";
+        }
+    }
+}
---
+++ flight-planner/Models/AirportRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace flight_planner.Models
+{
+    public class AirportRequest
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Airport { get; set; }
+
+        public override bool Equals(object obj)// kas tas pa objektu? Kāpēc metode ir Equals?
+        {
+            var airport = obj as AirportRequest;
+            if (airport == null)
+                return false;
+            return airport.Airport == Airport &&
+                   airport.City == City && 
+                   airport.Country == Country;
+        }
+
+
+
+    }
+}
