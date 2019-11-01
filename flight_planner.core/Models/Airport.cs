3004b252326b9f0ca3aaf109f25a12a50f64a41e
ausmoons <53189160+ausmoons@users.noreply.github.com>
2019-11-01 23:13:51 +0200

    Update Airport.cs

--- flight_planner.core/Models/Airport.cs
+++ flight_planner.core/Models/Airport.cs
@@ -12,17 +12,5 @@ namespace flight_planner.Models
         public string City { get; set; }
         public string AirportCode { get; set; }
 
-      /*  public override bool Equals(object obj)
-        {
-            var airport = obj as Airport;
-            if (airport == null)
-                return false;
-            return airport.AirportCode == AirportCode &&
-                   airport.City == City && 
-                   airport.Country == Country;
-        }*/
-
-
-
     }
 }
