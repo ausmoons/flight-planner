61dcdb19b31ca766b2b3461a5d1a3ad1893dc89a
ausmoons <53189160+ausmoons@users.noreply.github.com>
2019-11-01 23:14:23 +0200

    Update Flight.cs

--- flight_planner.core/Models/Flight.cs
+++ flight_planner.core/Models/Flight.cs
@@ -13,19 +13,5 @@ namespace flight_planner.Models
         public string Carrier { get; set; }
         public string DepartureTime { get; set; }
         public string ArrivalTime { get; set; }
-
-        /*public override bool Equals(object obj)
-        {
-            var result = false;
-            var flight = obj as Flight;
-            if (flight == null)
-            {
-                return false;
-
-            }
-            
-            return flight.Carrier == Carrier &&  flight.ArrivalTime == ArrivalTime &&   
-                    flight.DepartureTime == DepartureTime && flight.From.Equals(From) && flight.To.Equals(To);
-        }*/
     }
 }
